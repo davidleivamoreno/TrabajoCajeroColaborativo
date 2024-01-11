ca525b2869d46e3936cd0c7d6a21dcd8b8f004e9
Ismael Marfil <104906489+IsmaelMarfil@users.noreply.github.com>
2024-01-11 09:58:30 +0100

    Update CajeroV1.java

--- CajeroV1.java
+++ CajeroV1.java
@@ -3,6 +3,37 @@
 import java.math.BigDecimal;
 import java.util.Scanner;
 
+private static void escribirMovimiento(String movimiento) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("movimientos.txt", true))) {
+            writer.write(movimiento);
+            writer.newLine(); // Añade una nueva línea después de cada movimiento
+            System.out.println("Movimiento registrado en el archivo.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+    private static void verMovimientos() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("movimientos.txt"))) {
+            System.out.println("Movimientos registrados:");
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+    private static void borrarMovimientos() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("movimientos.txt", false))) {
+            // Sobrescribe el archivo con una cadena vacía
+            writer.write("");
+            System.out.println("Movimientos borrados exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error al borrar los movimientos: " + e.getMessage());
+        }
+    }
+
 public class CajeroV1 {
     public static void main(String[] args) {
         BigDecimal saldo = BigDecimal.valueOf(1000.0);
@@ -30,6 +61,8 @@ public class CajeroV1 {
                         saldo = saldo.subtract(cantidadRetiro);
                         System.out.println("Retiro exitoso. Saldo restante: " + saldo);
                     }
+                    escribirMovimiento("Retiro: " + cantidadRetiro);
+
                     break;
 
                 case 2:
@@ -39,13 +72,20 @@ public class CajeroV1 {
                     {
                         System.out.println("Operacion incorrecta introduce un numero positivo");
                     }
+
+                
                     else {
                         saldo = saldo.add(cantidadIngreso);
                         System.out.println("Ingreso exitoso. Saldo actual: " + saldo);
                     }
+                    escribirMovimiento("Ingreso: " + cantidadIngreso);
+                    break;
+                case 3:
+                    verMovimientos();
                     break;
 
                 case 0:
+                    borrarMovimientos();
                     System.out.println("Gracias por usar el cajero automático.");
                     break;
 
